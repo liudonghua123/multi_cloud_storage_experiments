48b6693e54b6a73cd93843775f1dd97260dfa563
liudonghua123 <liudonghua123@gmail.com>
2022-10-23 00:06:50 +0800

    enable init_logging level argument

--- common/config_logging.py
+++ common/config_logging.py
@@ -5,10 +5,10 @@ from os.path import dirname, join, realpath
 # config logging for both stdout and file
 
 
-def init_logging(file_name=join(dirname(realpath(__file__)), "app,log")):
+def init_logging(file_name=join(dirname(realpath(__file__)), "app,log"), level=logging.INFO):
     logging.basicConfig(
         format="%(levelname)s %(asctime)s - %(message)s",
-        level=logging.INFO,
+        level=level,
         handlers=[logging.FileHandler(file_name), logging.StreamHandler(sys.stdout)],
     )
     logger = logging.getLogger()
