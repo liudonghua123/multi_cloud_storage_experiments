c30294dd536c1e25f6fefd53176c5f55e9b96fef
liudonghua123 <liudonghua123@gmail.com>
2022-12-06 09:53:15 +0800

    update algorithm_simple.py

--- client/algorithm_simple.py
+++ client/algorithm_simple.py
@@ -14,7 +14,7 @@ from algorithm_common import *
 
 sys.path.append(dirname(dirname(realpath(__file__))))
 
-from common.utility import get_latency
+from common.utility import get_latency, get_latency_sync
 from common.config_logging import init_logging
 
 logger = init_logging(join(dirname(realpath(__file__)), "client.log"))
@@ -49,6 +49,7 @@ class EACH_SIMPLE:
         latency_cloud_timed = np.zeros((self.ticks, self.N))
         current_simple_latency = np.full((self.N,),np.inf)
         C_N_n_count = len(list(itertools.combinations(range(self.N), self.n)))
+        u_hat_it = np.zeros((self.N,))
         
         for tick, trace_data in enumerate(self.data):
             logger.info(f"[tick: {tick}]{'-'*20}")            
@@ -88,26 +89,37 @@ class EACH_SIMPLE:
             # do request to get latency
             # make a request to the cloud and save the latency to the latency_cloud_timed
             # if the passed cloud_placements is like [0,0,1,0,1,0], then the returned latency is like [0,0,35.12,0,28.75,0]
-            _, *latency_cloud = asyncio.run(get_latency(placement_policy, tick, self.N, self.k, cloud_providers, trace_data.file_size, trace_data.file_read))
+            trace_data.request_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+            _, *latency_cloud = get_latency_sync(placement_policy, tick, self.N, self.k, cloud_providers, trace_data.file_size, trace_data.file_read)
             logger.info(f"latency_cloud: {latency_cloud}")
             # update the latency of trace_data
             trace_data.latency = max(latency_cloud)
+            trace_data.latency_full = '   '.join(map(float_to_string, latency_cloud))
             trace_data.placement_policy = '_'.join([str(i) for i, x in enumerate(placement_policy) if x == 1])
             placement_policy_timed[tick] = placement_policy   
             latency_cloud_timed[tick] = latency_cloud
             
             # update the simple latency
-            choosed_clould_ids = [i for i, x in enumerate(placement_policy) if x == 1]
-            # choosed_clould_ids = np.where(placement_policy == 1)[0]
-            for clould_id in choosed_clould_ids:
-                current_simple_latency[clould_id] = latency_cloud[clould_id]
+            choosed_cloud_ids = [i for i, x in enumerate(placement_policy) if x == 1]
+            # choosed_cloud_ids = np.where(placement_policy == 1)[0]
+            for cloud_id in choosed_cloud_ids:
+                current_simple_latency[cloud_id] = latency_cloud[cloud_id]
+                
+            if trace_data.file_read:
+                post_reward = self.ψ1 * trace_data.latency + self.ψ2 * sum(map(lambda cloud_id: trace_data.file_size / 1024 / 1024 / 1024 / self.k * outbound_cost[cloud_id], choosed_cloud_ids))
+                trace_data.post_cost = sum(map(lambda cloud_id: trace_data.file_size / 1024 / 1024 / 1024 / self.k * outbound_cost[cloud_id], choosed_cloud_ids))
+            else: 
+                post_reward = self.ψ1 * trace_data.latency + self.ψ2 * sum(map(lambda cloud_id: trace_data.file_size / 1024 / 1024 / 1024 / self.k * storage_cost[cloud_id], choosed_cloud_ids))
+                trace_data.post_cost = sum(map(lambda cloud_id: trace_data.file_size / 1024 / 1024 / 1024 / self.k * storage_cost[cloud_id], choosed_cloud_ids))
+            trace_data.post_reward = post_reward
+            logger.info(f"tick: {tick}, post_cost: {trace_data.post_cost}, post_reward: {post_reward}")
             
         
     def save_result(self):
         # save the trace data with latency
         with open('results/trace_data_latency_simple.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            header = ['timestamp', 'file_id', 'file_size', 'file_read', 'latency', 'placement_policy']
+            header = ['timestamp', 'file_id', 'file_size', 'file_read', 'placement_policy', 'latency', 'latency_full', 'post_reward', 'post_cost', 'request_datetime']
             writer.writerow(header)
             for trace_data in self.data:
                 writer.writerow([getattr(trace_data, column) for column in header])
